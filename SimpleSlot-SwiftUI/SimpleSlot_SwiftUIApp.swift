107d377bd8e06e7d8c257aed89be4ebb7b428ba9
Alan <alansan96@hotmail.com>
2022-10-14 22:37:03 +0700

    Initial Commit

---
+++ SimpleSlot-SwiftUI/SimpleSlot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleSlot_SwiftUIApp.swift
+//  SimpleSlot-SwiftUI
+//
+//  Created by Alan Santoso on 14/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleSlot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
